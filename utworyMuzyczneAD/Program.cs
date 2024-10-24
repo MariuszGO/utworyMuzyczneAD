70a0f28ae4db784b1f82aca2c6eb7d86a915e1f8
Laptop 303 <Laptop 303@303SALA>
2024-10-24 13:04:19 +0200

    Dodaj pliki projektów.

---
+++ utworyMuzyczneAD/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        class albumy
+        {
+            string artist { get; set; }
+            string album { get; set; }
+            int songsNumber { get; set; }
+            int year { get; set; }
+            Int32 downloadNumber { get; set; }
+            public void odczytaj()
+            {
+                string plik = "Data.txt";
+                string linia;
+                int ilosc=0;
+
+                StreamReader sr = new StreamReader(plik);
+
+                while (!sr.EndOfStream) { 
+                
+                    ilosc++;
+                    
+
+                }
+
+
+                while (!sr.EndOfStream)
+                {
+                    linia = sr.ReadLine();
+                   ilosc++;
+                    Console.WriteLine(linia);
+                   
+                }
+                Console.WriteLine(ilosc);
+
+            }
+
+        };//koniec klasy albumy
+
+        
+    
+
+        static void Main(string[] args)
+        {
+            albumy album = new albumy();
+
+            album.odczytaj();
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
